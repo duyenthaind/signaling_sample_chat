9ae4f293630792954b80b87174fa45063e4ab6d0
duyenthaind <duyenthai123456@gmail.com>
2022-04-23 16:57:59 +0700

    feature - enable ssl

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const https = require('https');
+const fs = require("fs");
 const PORT = 8001 || process.env.PORT;
+const HOST = '0.0.0.0' || process.env.HOST;
 
-app.use('/src', express.static('src'))
+const key = fs.readFileSync("./cert.key");
+const cert = fs.readFileSync("./cert.crt");
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+const credentials = {
+  key: key,
+  cert: cert,
+};
+
+app.use("/src", express.static("src"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(PORT, function () {
-	console.log('listening on localhost:' + PORT);
+const server = https.createServer(credentials, app);
+server.listen(PORT, HOST, () => {
+	console.log("Server is start on address " + HOST + ":" + PORT + ", ssl/tls enabled");
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8088');
+const socket = io('https://localhost:8088');
 const accessTokenInput = document.querySelector('#accessToken-input');
 const userIdLabel = document.querySelector("#userId");
 const userIdInput = document.querySelector("#userId-input");
